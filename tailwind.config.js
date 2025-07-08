de981eb4ff3523b044e3e23976b02b12a4e7319c
ihyeeun <oioigl@naver.com>
2025-07-08 20:44:54 +0900

    UHYU-28 feat: 디자인 토큰 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'class',
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      fill: {
+        star: "var(--bg-star)", // SVG 내부 fill 색상
+      },
+    },
+  },
 };

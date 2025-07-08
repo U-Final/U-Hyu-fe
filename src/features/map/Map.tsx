f57566eb55b5c4054eb61118b8ff0da46a34d6ee
pillow12360 <ehdcks1256@naver.com>
2025-07-08 16:22:25 +0900

    UHYU-12 feat: Naver Maps 통합 및 테스트 렌더링

---
+++ src/features/map/Map.tsx
@@ -0,0 +1,13 @@
+import { NaverMap, Marker, useNavermaps } from 'react-naver-maps';
+
+function Map() {
+  // instead of window.naver.maps
+  const navermaps = useNavermaps();
+
+  return (
+    <NaverMap defaultCenter={new navermaps.LatLng(37.3595704, 127.105399)} defaultZoom={15}>
+      <Marker defaultPosition={new navermaps.LatLng(37.3595704, 127.105399)} />
+    </NaverMap>
+  );
+}
+export default Map;

769bac7c463d1aa2eb5f2ceef350f7279f910ac1
leedaye0412 <neoldy00@naver.com>
2025-07-09 10:06:23 +0900

    [UHYU-107] fix: 컴포넌트 폴더 구조 변경

--- src/components/SearchInput/__stories__/SearchInput.stories.ts
+++ src/components/SearchInput/__stories__/SearchInput.stories.ts
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
 
 import { createElement, useState } from "react";
-import SearchInput from "./SearchInput";
+import SearchInput from "../SearchInput";
 
 
 const meta: Meta<typeof SearchInput> = {

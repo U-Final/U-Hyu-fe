a614c5f7e4b824961e2629d2959c17b219689109
leedaye0412 <neoldy00@naver.com>
2025-07-10 21:03:57 +0900

    [UHYU-138] feat: BottomNavigation 컴포넌트 구현

---
+++ src/shared/components/bottom_navigation/BottomNavigation.tsx
@@ -0,0 +1,82 @@
+import { useState } from "react";
+import NavItem from "./NavItem";
+import { MdHomeFilled } from "react-icons/md";
+import { FaMap } from "react-icons/fa";
+import { HiGift } from "react-icons/hi";
+import { FaUser } from "react-icons/fa6";
+import { LiaBarcodeSolid } from "react-icons/lia";
+import BarcodeItem from "./bacodeItem";
+
+const BottomNavigation = () => {
+  const [activeTab, setActiveTab] = useState<string>("홈");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleTabClick = (tab: string) => {
+    setActiveTab(tab);
+  };
+
+  const handleBarcodeClick = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <div className="fixed bottom-0 w-full">
+      <nav className="h-12 text-[0.625rem] shadow-nav flex relative justify-around bg-white shadow-md gap-13 px-[1.5rem] py-[0.5rem]">
+        <div className="flex gap-4">
+          <NavItem
+            label="홈"
+            icon={<MdHomeFilled />}
+            isActive={activeTab === "홈"}
+            onClick={() => handleTabClick("홈")}
+          />
+          <NavItem
+            label="지도"
+            icon={<FaMap />}
+            isActive={activeTab === "지도"}
+            onClick={() => handleTabClick("지도")}
+          />
+        </div>
+
+        <BarcodeItem
+          label="바코드"
+          icon={<LiaBarcodeSolid />}
+          isActive={activeTab === "바코드"}
+          onClick={handleBarcodeClick}
+        />
+        <div className="flex gap-4">
+          <NavItem
+            label="혜택"
+            icon={<HiGift />}
+            isActive={activeTab === "혜택"}
+            onClick={() => handleTabClick("혜택")}
+          />
+          <NavItem
+            label="마이페이지"
+            icon={<FaUser />}
+            isActive={activeTab === "마이페이지"}
+            onClick={() => handleTabClick("마이페이지")}
+          />
+        </div>
+      </nav>
+      {isModalOpen && (
+        // 모달 컴포넌트 완성 후 수정해야함 그전에 임시 모달
+        <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center">
+          {isModalOpen && (
+            <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center">
+              <div className="bg-white p-5 rounded-lg">
+                <h2>바코드 스캔 모달</h2>
+                <button onClick={closeModal}>Close</button>
+              </div>
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default BottomNavigation;

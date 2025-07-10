6827ef5d1a48272c1a64fd7518875a741f6148b7
leedaye0412 <neoldy00@naver.com>
2025-07-10 22:44:56 +0900

    [UHYU-138] fix: 바코드 아이콘 크기 수정

--- src/shared/components/bottom_navigation/bacodeItem.tsx
+++ src/shared/components/bottom_navigation/bacodeItem.tsx
@@ -3,11 +3,11 @@ import type { NavItemProps } from "./NavItem.types";
 const BarcodeItem = ({ label, icon, onClick, isActive }: NavItemProps) => {
   return (
     <div
-      className={`w-12 h-12 absolute bottom-4 flex flex-col items-center justify-center text-white font- hover:bg-black hover:text-white rounded-full cursor-pointer transition-all duration-300 
+      className={`w-12 h-12 absolute bottom-4 shadow-nav flex flex-col items-center justify-center text-white font- hover:bg-black hover:text-white rounded-full cursor-pointer transition-all duration-300 
         ${isActive ? "bg-black" : "bg-gray"} `}
       onClick={onClick}
     >
-      <span className="">{icon}</span>
+      <span className="text-base">{icon}</span>
       <span className="mt-1">{label}</span>
     </div>
   );

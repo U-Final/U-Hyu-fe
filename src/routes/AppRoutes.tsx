ba11179972e07dae5b1b475ab8a0c6b08a2777d2
ihyeeun <oioigl@naver.com>
2025-07-10 22:34:29 +0900

    [UHYU-146] feat: 페이지 경로를 상수화 하는 구조 개선

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,5 +1,13 @@
-import { BrowserRouter } from "react-router-dom";
+import HomePage from "@/pages/HomePage";
+import { PATH } from "@shared/constants/path";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export const AppRoutes = () => {
-  return <BrowserRouter></BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={PATH.HOME} element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };

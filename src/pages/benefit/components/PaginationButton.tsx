e245e483f8a0411f2aa0a4d58ad79951607835e2
leedaye0412 <neoldy00@naver.com>
2025-07-11 18:23:29 +0900

    [UHYU-147] feat: 버튼 호버 추가

--- src/pages/benefit/components/PaginationButton.tsx
+++ src/pages/benefit/components/PaginationButton.tsx
@@ -5,7 +5,7 @@ const PaginationButton: FC<PaginationButtonProps> = ({ onClick, disabled, direct
   return (
     <button
       onClick={onClick}
-      className="px-6 py-2 bg-white text-gray border border-gray rounded-sm disabled:opacity-50 cursor-pointer"
+      className="px-6 py-2 bg-white text-gray border border-gray rounded-sm disabled:opacity-50 cursor-pointer hover:bg-gray-hover"
       disabled={disabled}
     >
       {direction === "prev" ? "<" : ">"}

b498c75a7d0df391816b1a04077e2379ebc74b0d
leedaye0412 <neoldy00@naver.com>
2025-07-15 18:01:32 +0900

    [UHYU-168] refactor: PrimaryButton import문 공통컴포넌트로 수정 및 div 삭제

--- src/features/mymap/components/StoreDeleteModal.tsx
+++ src/features/mymap/components/StoreDeleteModal.tsx
@@ -1,5 +1,5 @@
-import BaseModal from "@components/modals/BaseModal";
-import { PrimaryButton } from "@extra-info/components/PrimaryButton";
+import { PrimaryButton } from '@components/buttons/PrimaryButton';
+import BaseModal from '@components/modals/BaseModal';
 
 interface StoreDeleteModalProps {
   onConfirm: () => void;
@@ -8,8 +8,8 @@ interface StoreDeleteModalProps {
 export const StoreDeleteModal = ({ onConfirm }: StoreDeleteModalProps) => {
   return (
     <BaseModal title="매장 삭제">
-      <p className="text-sm text-black">이 매장을 리스트에서 삭제합니다</p>
-      <div className="mt-4 flex justify-end gap-2">
+      <div className='flex flex-col gap-5'>
+        <p className="text-sm text-black">이 매장을 리스트에서 삭제합니다</p>
         <PrimaryButton onClick={onConfirm}>삭제</PrimaryButton>
       </div>
     </BaseModal>

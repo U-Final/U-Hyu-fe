b566a761ee5b915ff5552030772d7b3d76b53f6f
leedaye0412 <neoldy00@naver.com>
2025-07-16 16:09:33 +0900

    [UHYU-164] feat: 크롭된 바코드 이미지를 표시하는 컴포넌트 추가

---
+++ src/shared/components/bottom_navigation/barcode/CroppedImg.tsx
@@ -0,0 +1,17 @@
+import type { FC } from "react";
+
+interface CroppedImgProps {
+  image: string;
+}
+
+export const CroppedImg: FC<CroppedImgProps> = ({ image }) => {
+  return (
+    <figure>
+      <img
+        src={image}
+        alt="크롭된 바코드 이미지"
+        className="max-w-full max-h-48 object-contain"
+      />
+    </figure>
+  );
+};

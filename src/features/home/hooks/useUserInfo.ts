4de27831418cde7df97185732c68b3714d64c3f5
ihyeeun <oioigl@naver.com>
2025-07-21 01:56:02 +0900

    [UHYU-155] chore: 필요없는 부분 삭제

--- src/features/home/hooks/useUserInfo.ts
+++ src/features/home/hooks/useUserInfo.ts
@@ -1,11 +1,10 @@
 // src/features/user/hooks/useUserInfoQuery.ts
 import type { UserInfo } from '@/features/home/api/homeApi';
 import { fetchUserInfo } from '@/features/home/api/homeApi';
-import type { ApiErrorResponse } from '@/shared/client/client.type';
 import { useQuery } from '@tanstack/react-query';
 
 export const useUserInfoQuery = () =>
-  useQuery<UserInfo, ApiErrorResponse>({
+  useQuery<UserInfo>({
     queryKey: ['userInfo'],
     queryFn: fetchUserInfo,
   });

4e7008f263f17f740af23fc5839f5f11ba4a3ad8
ihyeeun <oioigl@naver.com>
2025-07-20 18:51:26 +0900

    [UHYU-155] feat: home 관련 bottomContents 내용 넣어지는 거 간단 구현

--- src/features/home/components/auth/AuthHeader.tsx
+++ src/features/home/components/auth/AuthHeader.tsx
@@ -1,7 +1,15 @@
+import { useUserInfoQuery } from "@home/hooks/useUserInfo";
+
 export const AuthHeader = () => {
+  const { data: user, isLoading, isError } = useUserInfoQuery();
+  
+  if (isLoading) return <p>로딩 중...</p>;
+  if (isError || !user) return <p>로그인된 유저가 아닙니다.</p>;
+  
   return (
     <div>
-      <h1>로그인한 사용자에게 맞춤형 정보를 제공!</h1>
+      <h2>{user.user_name}님, 안녕하세요!</h2>
+      <p>당신의 등급: {user.grade}</p>
     </div>
   );
 };

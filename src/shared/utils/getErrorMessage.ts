68c736a0f1256377d612663e7752c089af330567
ihyeeun <oioigl@naver.com>
2025-07-21 11:37:21 +0900

    [UHYU-197] refactor: 공통 에러 응답 형식 수정

--- src/shared/utils/getErrorMessage.ts
+++ src/shared/utils/getErrorMessage.ts
@@ -1,8 +1,18 @@
-import { ApiErrorResponse } from "@/shared/client/ApiErrorResponse";
-
+import type { ApiError } from "@/shared/client/client.type";
 
 export const getErrorMessage = (error: unknown): string => {
-  if (error instanceof ApiErrorResponse) return error.message;
-  if (error instanceof Error) return error.message;
+  if (
+    typeof error === 'object' &&
+    error !== null &&
+    'message' in error &&
+    typeof (error as Record<string, unknown>).message === 'string'
+  ) {
+    return (error as ApiError).message;
+  }
+
+  if (error instanceof Error) {
+    return error.message;
+  }
+
   return '알 수 없는 오류가 발생했습니다.';
 };

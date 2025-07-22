98a729a9d7e6d2fa0f97bccd29f8d31cc84ef05c
ihyeeun <oioigl@naver.com>
2025-07-22 21:06:50 +0900

    [UHYU-205] feat: recommendation MSW 설정

--- src/shared/msw/handlers/index.ts
+++ src/shared/msw/handlers/index.ts
@@ -1,5 +1,11 @@
 import { benefitHandlers } from './benefit';
 import { homeHandlers } from './home';
 import { mapHandlers } from './map';
+import { recommendHandlers } from './recommendation';
 
-export const handlers = [...mapHandlers, ...homeHandlers, ...benefitHandlers];
+export const handlers = [
+  ...mapHandlers,
+  ...homeHandlers,
+  ...benefitHandlers,
+  ...recommendHandlers,
+];

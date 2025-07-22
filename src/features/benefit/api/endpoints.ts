f6ddebda32c481e8c3060a91c12922155fb266b6
leedaye0412 <neoldy00@naver.com>
2025-07-22 13:49:35 +0900

    [UHYU-52] feat: 제휴처 정보 상세 조회 MSW용 endpoint 및  MSW 핸들러

--- src/features/benefit/api/endpoints.ts
+++ src/features/benefit/api/endpoints.ts
@@ -3,6 +3,7 @@ const BENEFIT = '/brand-list';
 export const BENEFIT_ENDPOINTS = {
   BENEFIT: {
     ROOT: BENEFIT,
+    DETAIL_MSW: '/brand-list/:brandId',
     DETAIL: (brandId: number) => `${BENEFIT}/${brandId}`,
   },
 };
--- src/shared/msw/handlers/benefit.ts
+++ src/shared/msw/handlers/benefit.ts
@@ -1,12 +1,19 @@
 import { BENEFIT_ENDPOINTS } from '@benefit/api/endpoints';
-import { allBrands } from '@benefit/api/mockData';
+import { allBrands, mockBrandDetails } from '@benefit/api/mockData';
 import type { BenefitType, BrandListRes, StoreType } from '@benefit/api/types';
 import { http } from 'msw';
+
+
 
 import { createErrorResponse } from '@/shared/utils/createErrorResponse';
 import { createResponse } from '@/shared/utils/createResponse';
 
+
+
+
+
 export const benefitHandlers = [
+  // 제휴처 목록 조회 MSW 핸들러
   http.get(BENEFIT_ENDPOINTS.BENEFIT.ROOT, ({ request }) => {
     const url = new URL(request.url);
     const shouldFail = false;
@@ -74,5 +81,21 @@ export const benefitHandlers = [
 
       return createResponse(result, '제휴처 리스트 목록 api 성공');
     }
+  }),
+
+  // 제휴처 정보 상세 조회 MSW 핸들러
+  http.get(BENEFIT_ENDPOINTS.BENEFIT.DETAIL_MSW, ({ params }) => {
+    const { brandId } = params;
+
+    const shouldFail = false;
+    if (shouldFail) {
+      //임시 에러메시지
+      return createErrorResponse('에러메시지', 404);
+    }
+
+    const brand = mockBrandDetails.find(b => b.brandId === Number(brandId));
+  if (!brand) {
+    return createResponse(mockBrandDetails, '유저 정보 조회 성공');
+  }
   }),
 ];

e33c4a278fa9a9dba1c8e01ba0e59d7ad0f0cf75
leedaye0412 <neoldy00@naver.com>
2025-07-22 10:53:35 +0900

    [UHYU-52] feat: 제휴처 정보 상세 조회 endpoint

--- src/features/benefit/api/endpoints.ts
+++ src/features/benefit/api/endpoints.ts
@@ -3,6 +3,6 @@ const BENEFIT = '/brand-list';
 export const BENEFIT_ENDPOINTS = {
   BENEFIT: {
     ROOT: BENEFIT,
-    // BENEFIT_DETAIL: `${BENEFIT}/{brand_id}`, //혜택 상세 api 들어갈 예정
+    DETAIL: (brandId: number) => `${BENEFIT}/${brandId}`,
   },
 };

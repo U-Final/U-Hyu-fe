40050ef2e7cbe2c3a53489fc7d0cf2c6f01b632d
ihyeeun <oioigl@naver.com>
2025-07-23 16:09:24 +0900

    [UHYU-204] chore: 홈페이지 필요없는 부분 내용 삭제, home 관련 파일은 추후 삭제 예정

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,19 +1,15 @@
-import {
-  AuthHomeSheetContent,
-  GuestHomeSheetContent,
-} from '@home/components';
 import { LoginButton } from '@home/components/LoginButton';
 
 import { DragBottomSheet } from '@/shared/components';
 
 const HomePage = () => {
-  const isLoggedIn = true; // 로그인 여부를 확인하는 로직이 필요합니다.
+  // const isLoggedIn = true; // 로그인 여부를 확인하는 로직이 필요합니다.
   return (
     <div>
       <div></div>
       <DragBottomSheet>
         <LoginButton />
-        {isLoggedIn ? <AuthHomeSheetContent /> : <GuestHomeSheetContent />}
+        {/* {isLoggedIn ? <AuthHomeSheetContent /> : <GuestHomeSheetContent />} */}
       </DragBottomSheet>
     </div>
   );

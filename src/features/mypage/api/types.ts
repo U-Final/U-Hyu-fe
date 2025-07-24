8d2475ce47c1b8bb3edb3e72200796ef6f37a15b
pillow12360 <ehdcks1256@naver.com>
2025-07-24 14:19:25 +0900

    [UHYU-234] refactor: 타입 명세에 맞게 수정

--- src/features/mypage/api/types.ts
+++ src/features/mypage/api/types.ts
@@ -18,7 +18,7 @@ export interface UserInfo {
   nickName: string;
   kakaoId: number;
   email: string;
-  age: string;
+  age: number;
   gender: Gender;
   role: UserRole;
   status: UserStatus;
@@ -45,7 +45,7 @@ export interface UserInfoResponse {
     userName: string;
     nickName: string;
     email: string;
-    age: string;
+    age: number;
     gender: Gender;
     grade: UserGrade;
     updatedAt: string;

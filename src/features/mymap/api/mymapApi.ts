1204ac13b2c856a62aaaa111f073ffadba323a49
leedaye0412 <neoldy00@naver.com>
2025-07-24 14:52:52 +0900

    [UHYU-224] feat: my map 추가, 수정, 삭제 API 함수 구현

--- src/features/mymap/api/mymapApi.ts
+++ src/features/mymap/api/mymapApi.ts
@@ -2,7 +2,15 @@ import { client } from '@/shared/client';
 import type { ApiResponse } from '@/shared/client/client.type';
 
 import { MYMAP_ENDPOINTS } from './endpoint';
-import type { MyMapListRes } from './types';
+import type {
+  MyMapListDeleteParams,
+  MyMapListDeleteRes,
+  MyMapListRes,
+  MyMapListUpdateReq,
+  MyMapListUpdateRes,
+  MyMapStoreAddReq,
+  MyMapStoreAddRes,
+} from './types';
 
 // My Map 목록 조회 api
 export const getMyMapList = async (): Promise<MyMapListRes[]> => {
@@ -14,4 +22,36 @@ export const getMyMapList = async (): Promise<MyMapListRes[]> => {
     throw new Error('MyMap 목록 데이터를 불러올 수 없습니다');
   }
   return res.data.data;
+};
+
+// My Map 추가 API
+export const addMyMap = async (
+  body: MyMapStoreAddReq
+): Promise<MyMapStoreAddRes> => {
+  const res = await client.post<MyMapStoreAddRes>(
+    MYMAP_ENDPOINTS.MYMAP.ROOT,
+    body
+  );
+  return res.data;
+};
+
+// My Map 수정 api
+export const updateMyMap = async (
+  body: MyMapListUpdateReq
+): Promise<MyMapListUpdateRes> => {
+  const res = await client.patch<MyMapListUpdateRes>(
+    MYMAP_ENDPOINTS.MYMAP.ROOT,
+    body
+  );
+  return res.data;
+};
+
+// My Map 삭제 API
+export const deleteMyMap = async (
+  myMapListId: MyMapListDeleteParams['myMapListId']
+): Promise<MyMapListDeleteRes> => {
+  const res = await client.delete<MyMapListDeleteRes>(
+    MYMAP_ENDPOINTS.MYMAP.DELETE(myMapListId)
+  );
+  return res.data;
 };

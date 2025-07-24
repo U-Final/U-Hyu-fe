9a4d7614e62d808fd98246889f2ffc5065389fd3
ihyeeun <oioigl@naver.com>
2025-07-24 19:41:10 +0900

    [UHYU-231] feat: 바코드 비로그인사용자 컴포넌트 구현

--- src/shared/components/bottom_navigation/barcode/contents/GuestBarcodeSection.tsx
+++ src/shared/components/bottom_navigation/barcode/contents/GuestBarcodeSection.tsx
@@ -1,9 +1,17 @@
-const GuestBarcodeContent = () => {
+import { NavButton } from '@/shared/components/buttons/NavButton';
+
+export const GuestBarcodeContent = () => {
   return (
-    <div>
-      <h2>로그인이 필요합니다!</h2>
+    <div className="flex flex-col w-full gap-4">
+      <p className="text-lg text-black">
+        멤버십 바코드 기능은 로그인 후 이용하실 수 있어요.
+      </p>
+      <NavButton
+        to={import.meta.env.VITE_KAKAO_LOGIN_URL}
+        className="w-full text-lg"
+      >
+        로그인 하러가기
+      </NavButton>
     </div>
   );
 };
-
-export default GuestBarcodeContent;

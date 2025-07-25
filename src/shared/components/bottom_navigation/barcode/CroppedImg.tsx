a4b5acff1f0e9975cfa51730e37f803bee6b3e60
ihyeeun <oioigl@naver.com>
2025-07-25 11:16:50 +0900

    [UHYU-231] refactor: 바코드 이미지 최대 높이 수정

--- src/shared/components/bottom_navigation/barcode/CroppedImg.tsx
+++ src/shared/components/bottom_navigation/barcode/CroppedImg.tsx
@@ -10,7 +10,7 @@ export const CroppedImg: FC<CroppedImgProps> = ({ image }) => {
       <img
         src={image}
         alt="크롭된 바코드 이미지"
-        className="max-w-full max-h-200 object-contain w-full"
+        className="max-w-full max-h-[600px] object-contain w-full"
       />
     </figure>
   );

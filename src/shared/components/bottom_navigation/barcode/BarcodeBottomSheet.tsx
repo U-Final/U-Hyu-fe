dbbed793b4551e6857157717d03bed376bfd9e2a
ihyeeun <oioigl@naver.com>
2025-07-25 11:17:11 +0900

    [UHYU-231] chore: 조건부 수정

--- src/shared/components/bottom_navigation/barcode/BarcodeBottomSheet.tsx
+++ src/shared/components/bottom_navigation/barcode/BarcodeBottomSheet.tsx
@@ -60,7 +60,7 @@ export const BarcodeBottomSheet: FC<BarcodeBottomSheetProps> = ({
             </button>
           </header>
 
-          {!isLoggedIn ? <LoggedInBarcodeContent /> : <GuestBarcodeContent />}
+          {isLoggedIn ? <LoggedInBarcodeContent /> : <GuestBarcodeContent />}
         </div>
       </div>
     </>

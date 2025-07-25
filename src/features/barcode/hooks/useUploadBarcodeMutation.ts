76f26fcb858e4a64baea6034ac718dccb6d995a4
ihyeeun <oioigl@naver.com>
2025-07-25 15:46:41 +0900

    [UHYU-231] feat: 바코드 이미지 캐싱 구현

--- src/features/barcode/hooks/useUploadBarcodeMutation.ts
+++ src/features/barcode/hooks/useUploadBarcodeMutation.ts
@@ -1,8 +1,14 @@
+import { BARCODE_IMAGE_QUERY_KEY } from '@barcode/api/barcode.type';
 import { postUploadBarcodeImage } from '@barcode/api/image-upload/barcodeImage.Api';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 export const useUploadBarcodeMutation = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: postUploadBarcodeImage,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: BARCODE_IMAGE_QUERY_KEY });
+    },
   });
 };

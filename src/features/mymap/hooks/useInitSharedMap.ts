fa8abc07ce53d1dd46675beb65d5ab5b3e5eac1a
leedaye0412 <neoldy00@naver.com>
2025-07-28 04:52:28 +0900

    [UHYU-249] feat: useInitSharedMap.ts 훅

---
+++ src/features/mymap/hooks/useInitSharedMap.ts
@@ -0,0 +1,36 @@
+import { useEffect } from 'react';
+
+import { getMyMapUuid } from '@mymap/api/mymapApi';
+
+import { useSharedMapStore } from '../store/SharedMapStore';
+
+export const useInitSharedMap = (uuid?: string) => {
+  const setSharedMap = useSharedMapStore(state => state.setSharedMap);
+  const clearSharedMap = useSharedMapStore(state => state.clearSharedMap);
+
+  useEffect(() => {
+    if (!uuid) {
+      clearSharedMap();
+      return;
+    }
+
+    const fetchSharedMap = async () => {
+      try {
+        const data = await getMyMapUuid(uuid);
+        const sharedMapData = {
+          ...data,
+          stores: data.storeList.map(store => ({
+            ...store,
+            benefit: store.benefit ?? '',
+          })),
+        };
+        setSharedMap(sharedMapData);
+      } catch (error) {
+        console.error('공유 지도 불러오기 실패:', error);
+        clearSharedMap();
+      }
+    };
+
+    fetchSharedMap();
+  }, [uuid, setSharedMap, clearSharedMap]);
+};

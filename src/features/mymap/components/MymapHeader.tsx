7c939c673148faea3a55060fd0511857822fa56f
leedaye0412 <neoldy00@naver.com>
2025-07-28 17:20:42 +0900

    [UHYU-277] feat: mymappage 구현

---
+++ src/features/mymap/components/MymapHeader.tsx
@@ -0,0 +1,8 @@
+export const MymapHeader = () => (
+  <div className="flex flex-col gap-4">
+    <p className="text-h3 font-bold text-black">MyMap</p>
+    <p className="text-caption text-black">
+      마이맵을 통해 나만의 제휴처 지도를 만들어보세요.
+    </p>
+  </div>
+);

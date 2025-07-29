80818f37014e37d859df98219be91b1cbea9be7d
ihyeeun <oioigl@naver.com>
2025-07-30 06:54:58 +0900

    [UHYU-300] feat: 에러시 서버 메시지 토스트창 뜨게 구현

--- src/shared/client/axiosClient.ts
+++ src/shared/client/axiosClient.ts
@@ -1,4 +1,5 @@
 import axios, { type AxiosInstance } from 'axios';
+import { toast } from 'sonner';
 
 import { userStore } from '@/shared/store/userStore';
 
@@ -18,10 +19,8 @@ const responseInterceptor = (instance: AxiosInstance) => {
 
       // ✅ 1. 인증 만료 처리 (401 에러 발생 시)
       if (res?.status === 401) {
-        console.warn('🛑 인증 만료됨 - 자동 로그아웃 처리');
-
         userStore.getState().clearUser(); // Zustand 상태 초기화
-        alert('로그인이 만료되었습니다. 다시 로그인해주세요.');
+        toast.error('로그인이 만료되었습니다. 다시 로그인해주세요.');
         return Promise.reject({
           statusCode: 401,
           message: '인증이 만료되었습니다.',
@@ -30,6 +29,7 @@ const responseInterceptor = (instance: AxiosInstance) => {
 
       // ✅ 2. 서버에서 내려준 에러 응답 처리
       if (res?.data?.statusCode && res?.data?.message) {
+        toast.error(res.data.message);
         return Promise.reject({
           statusCode: res.data.statusCode,
           message: res.data.message,
@@ -37,6 +37,7 @@ const responseInterceptor = (instance: AxiosInstance) => {
       }
 
       // ✅ 3. 예외 케이스 (네트워크 등)
+      toast.error('서버와의 연결에 실패했습니다.');
       return Promise.reject({
         statusCode: 500,
         message: '서버와의 연결에 실패했습니다.',

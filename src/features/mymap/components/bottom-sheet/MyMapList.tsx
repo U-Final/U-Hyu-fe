f2741ee5fae649beb8dc2a41937240625249a0ee
leedaye0412 <neoldy00@naver.com>
2025-07-29 22:08:44 +0900

    [UHYU-289] feat: cursor-pointer 추가

--- src/features/mymap/components/bottom-sheet/MyMapList.tsx
+++ src/features/mymap/components/bottom-sheet/MyMapList.tsx
@@ -123,7 +123,7 @@ const MyMapList: FC = () => {
                   onClick={() => {
                     handleUpdate(map.myMapListId, map.title, map.markerColor);
                   }}
-                  className="flex justify-between font-medium"
+                  className="flex justify-between font-medium cursor-pointer"
                 >
                   수정
                   <RiPencilFill className="mr-2 h-4 w-4" />
@@ -134,7 +134,7 @@ const MyMapList: FC = () => {
                     e.stopPropagation();
                     handleShare(map.uuid);
                   }}
-                  className="flex justify-between font-medium"
+                  className="flex justify-between font-medium cursor-pointer"
                 >
                   공유
                   <MdIosShare className="mr-2 h-4 w-4" />
@@ -145,7 +145,7 @@ const MyMapList: FC = () => {
                     e.stopPropagation();
                     handleDelete(map.myMapListId);
                   }}
-                  className="flex justify-between font-medium"
+                  className="flex justify-between font-medium cursor-pointer"
                 >
                   삭제
                   <PiTrashBold className="mr-2 h-4 w-4" />

ebb58e24094197643a45aa642261bb3a64689939
leedaye0412 <neoldy00@naver.com>
2025-07-29 12:37:37 +0900

    [UHYU-288] feat: 공유 카드 설정

---
+++ src/features/mymap/hooks/useKakaoShare.ts
@@ -0,0 +1,43 @@
+import { useEffect } from 'react';
+
+
+
+
+
+const { Kakao } = window;
+
+export const useKakaoShare = () => {
+  useEffect(() => {
+    const key = import.meta.env.VITE_KAKAO_JAVASCRIPT_KEY;
+
+    Kakao.cleanup();
+    Kakao.init(key);
+  }, []);
+
+  const share = () => {
+    Kakao.Share.sendDefault({
+      objectType: 'feed',
+      content: {
+        title: 'u-hyu',
+        description: 'U+ 제휴처 공유 지도 도착! 혜택 받기 전, 먼저 확인하세요 ',
+        imageUrl:
+          'https://www.u-hyu.site/images/share.png',
+        link: {
+          mobileWebUrl: location.href,
+          webUrl: location.href,
+        },
+      },
+
+      buttons: [
+        {
+          title: '지도 보러가기',
+          link: {
+            mobileWebUrl: location.href,
+            webUrl: location.href,
+          },
+        },
+      ],
+    });
+  };
+  return share;
+};

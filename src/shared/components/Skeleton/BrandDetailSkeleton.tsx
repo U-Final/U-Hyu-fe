d8a933c42dc555019a1a4bb3b0f693e4d37c869a
leedaye0412 <neoldy00@naver.com>
2025-07-31 16:00:37 +0900

    [UHYU-326] fix: 스켈레톤 색상 넣기

--- src/shared/components/Skeleton/BrandDetailSkeleton.tsx
+++ src/shared/components/Skeleton/BrandDetailSkeleton.tsx
@@ -1,22 +1,26 @@
 import { Skeleton } from '../shadcn/ui/skeleton';
 
+
+
+
+
 export const BrandDetailSkeleton = () => (
   <div className="flex flex-col gap-4 text-black text-caption">
     {/* 브랜드명 스켈레톤 */}
-    <Skeleton className="h-6 w-32" />
+    <Skeleton className="h-6 w-32 bg-light-gray" />
 
     {/* 등급별 혜택 스켈레톤 */}
     <div className="flex flex-col gap-1">
-      <Skeleton className="h-4 w-20" />
+      <Skeleton className="h-4 w-20 bg-light-gray" />
       <div className="flex flex-col gap-1">
         {/* 3개의 등급 스켈레톤 */}
         {[1, 2, 3].map(i => (
           <div
             key={i}
-            className="flex justify-between items-center px-3 py-2 rounded-md bg-gray-100"
+            className="flex justify-between items-center px-3 py-2 rounded-md"
           >
-            <Skeleton className="h-4 w-10" />
-            <Skeleton className="h-4 w-40" />
+            <Skeleton className="h-4 w-10 bg-light-gray" />
+            <Skeleton className="h-4 w-40 bg-light-gray" />
           </div>
         ))}
       </div>
@@ -24,18 +28,18 @@ export const BrandDetailSkeleton = () => (
 
     {/* 제공 횟수 스켈레톤 */}
     <div className="flex flex-col gap-1">
-      <Skeleton className="h-4 w-16" />
-      <Skeleton className="h-4 w-24" />
+      <Skeleton className="h-4 w-16 bg-light-gray" />
+      <Skeleton className="h-4 w-24 bg-light-gray" />
     </div>
 
     {/* 이용방법 스켈레톤 */}
     <div className="flex flex-col gap-1">
-      <Skeleton className="h-4 w-16" />
+      <Skeleton className="h-4 w-16 bg-light-gray" />
       <div className="flex flex-col gap-2">
-        <Skeleton className="h-4 w-full" />
-        <Skeleton className="h-4 w-5/6" />
-        <Skeleton className="h-4 w-4/5" />
-        <Skeleton className="h-4 w-3/4" />
+        <Skeleton className="h-4 w-full bg-light-gray" />
+        <Skeleton className="h-4 w-5/6 bg-light-gray" />
+        <Skeleton className="h-4 w-4/5 bg-light-gray" />
+        <Skeleton className="h-4 w-3/4 bg-light-gray" />
       </div>
     </div>
   </div>

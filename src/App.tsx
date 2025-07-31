d6c3e1b9ef61fc60b01bd8e20f837f436ea19879
leedaye0412 <neoldy00@naver.com>
2025-07-31 13:55:59 +0900

    [UHYU-318] feat: 토스트 렌더링

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Toaster } from 'sonner';
 
 import { AppRoutes } from '@/routes/AppRoutes';
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <AppRoutes />
+      <Toaster />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );

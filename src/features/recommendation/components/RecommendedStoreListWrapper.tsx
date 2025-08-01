e5fa82857a2a6059cad8042a0e25e4eeae19a283
ihyeeun <oioigl@naver.com>
2025-08-01 21:09:52 +0900

    [UHYU-67] chore: recommend 필요없는 부분 삭제

--- src/features/recommendation/components/RecommendedStoreListWrapper.tsx
+++ src/features/recommendation/components/RecommendedStoreListWrapper.tsx
@@ -8,15 +8,6 @@ import { useAuthState } from '@/shared/store/userStore';
 export const RecommendedStoreListWrapper = () => {
   const { user } = useAuthState();
 
-  // if (!isAuthChecked) {
-  //   return (
-  //     <div className="flex flex-col justify-center items-center">
-  //       <BeatLoader size={10} />
-  //       <p className="text-gray">유저 인증 확인 중...</p>
-  //     </div>
-  //   );
-  // }
-
   return user ? (
     <RecommendedStoreListLoggedIn />
   ) : (

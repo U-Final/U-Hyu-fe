bf5ad16124a7a5b0f2f7d6450068e469f05148f0
ihyeeun <oioigl@naver.com>
2025-08-01 21:00:12 +0900

    [UHYU-67] feat: 추천 카드 로딩창 없애기

--- src/features/recommendation/components/RecommendedStoreListWrapper.tsx
+++ src/features/recommendation/components/RecommendedStoreListWrapper.tsx
@@ -2,21 +2,20 @@ import {
   RecommendedStoreListGuest,
   RecommendedStoreListLoggedIn,
 } from '@recommendation/components';
-import { BeatLoader } from 'react-spinners';
 
 import { useAuthState } from '@/shared/store/userStore';
 
 export const RecommendedStoreListWrapper = () => {
-  const { user, isAuthChecked } = useAuthState();
+  const { user } = useAuthState();
 
-  if (!isAuthChecked) {
-    return (
-      <div className="flex flex-col justify-center items-center">
-        <BeatLoader size={10} />
-        <p className="text-gray">유저 인증 확인 중...</p>
-      </div>
-    );
-  }
+  // if (!isAuthChecked) {
+  //   return (
+  //     <div className="flex flex-col justify-center items-center">
+  //       <BeatLoader size={10} />
+  //       <p className="text-gray">유저 인증 확인 중...</p>
+  //     </div>
+  //   );
+  // }
 
   return user ? (
     <RecommendedStoreListLoggedIn />

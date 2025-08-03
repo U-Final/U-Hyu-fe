3c7a9b786fa9a9290fb79fa98a6cd9ec01ad009e
pillow12360 <ehdcks1256@naver.com>
2025-08-03 18:22:52 +0900

    [UHYU-361] fix: 내위치로 이동 로직 한번만 적용되는 버그 해결 (캐시 제거)

--- src/features/kakao-map/components/location/LocationControlContainer.tsx
+++ src/features/kakao-map/components/location/LocationControlContainer.tsx
@@ -5,9 +5,13 @@ import LocationButton from './LocationButton';
 export const LocationControlContainer: React.FC = () => {
   const { getCurrentLocation, loading } = useMapData();
 
+  const handleLocationClick = () => {
+    getCurrentLocation(true); // force: true로 강제 실행
+  };
+
   return (
     <LocationButton
-      onClick={getCurrentLocation}
+      onClick={handleLocationClick}
       isLoading={loading.location}
     />
   );

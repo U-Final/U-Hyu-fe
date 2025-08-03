0d83d94dceda7651de4d8d232c2cedb50e867929
pillow12360 <ehdcks1256@naver.com>
2025-08-03 23:29:32 +0900

    [UHYU-367] chore: benefit 페이지 반응형 처리

--- src/pages/benefit/BenefitPage.tsx
+++ src/pages/benefit/BenefitPage.tsx
@@ -2,7 +2,7 @@ import { BenefitHeader, BenefitList } from '@benefit/components';
 
 const BenefitPage = () => {
   return (
-    <div className="flex flex-col gap-4 pb-10">
+    <div className="flex flex-col gap-4 pb-10 pt-6 sm:pt-2">
       <BenefitHeader />
       <BenefitList />
     </div>

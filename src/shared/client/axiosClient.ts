cd1ef3d4adaa69aa402b8b9a9054b23b3362ff11
ihyeeun <oioigl@naver.com>
2025-08-06 21:46:07 +0900

    feat: 바코드 모달 열었을때 이미지 없을때 토스트창 안뜨게 추가

--- src/shared/client/axiosClient.ts
+++ src/shared/client/axiosClient.ts
@@ -38,7 +38,9 @@ const responseInterceptor = (instance: AxiosInstance) => {
 
       // ✅ 2. 서버에서 내려준 에러 응답 처리
       if (res?.data?.statusCode && res?.data?.message) {
-        toast.error(res.data.message);
+        if (!error.config?.meta?.suppressErrorToast) {
+          toast.error(res.data.message);
+        }
         return Promise.reject({
           statusCode: res.data.statusCode,
           message: res.data.message,
